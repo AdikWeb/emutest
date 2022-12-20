dbdde9e8ff898923f394e9721d0c151487a9bbf0
GT <mr.tazhibayev@gmail.com>
2022-12-20 00:41:59 -0500

    Вытащил все в новый класс и добавил комнаты.

---
+++ src/Game.js
@@ -0,0 +1,87 @@
+const wasm = require('./wasm/genplus.js');
+const fs = require('fs');
+const { createCanvas } = require('canvas');
+
+fs.readFileSync(__dirname+'/wasm/genplus.wasm');
+
+class Game {
+    constructor() {
+        this.canvas = createCanvas(320, 240);
+        this.canvasContext = this.canvas.getContext('2d');
+        this.canvasContext.fillStyle = "#00FFFF";
+        this.canvasContext.font = "12px monospace";
+        this.canvasImageData = this.canvasContext.createImageData(320, 240);
+        this.fps = 0;
+        this.frame = 60;
+        this.startTime = new Date().getTime();
+        this.initialized = false;
+        this.pause = false;
+        this.now = 0;
+        this.then = 0;
+        this.delta = 0;
+        this.audio_l = 0;
+        this.audio_r = 0;
+        this.romdata = null;
+        this.vram = null;
+        this.gens = null;
+        this.ROM_PATH = './assets/roms/mk/8.bin';
+        this.CANVAS_WIDTH = 320;
+        this.CANVAS_HEIGHT = 240;
+        this.SAMPLING_PER_FPS = 736;
+        this.GAMEPAD_API_INDEX = 32;
+        this.FPS = 60;
+        this.INTERVAL = 1000 / this.FPS;
+        this.init();
+    }
+
+    init() {
+        wasm().then(module => {
+            this.gens = module;
+            this.gens._init();
+            const bytes = fs.readFileSync(this.ROM_PATH);
+            this.romdata = new Uint8Array(this.gens.HEAPU8.buffer, this.gens._get_rom_buffer_ref(bytes.byteLength), bytes.byteLength);
+            this.romdata.set(new Uint8Array(bytes));
+            this.initialized = true;
+            this.start();
+        });
+    }
+
+    start() {
+        if (this.initialized) {
+            this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.gens._start();
+            // vram view
+            this.vram = new Uint8ClampedArray(this.gens.HEAPU8.buffer, this.gens._get_frame_buffer_ref(), this.CANVAS_WIDTH * this.CANVAS_HEIGHT * 4);
+            // audio view
+            this.audio_l = new Float32Array(this.gens.HEAPF32.buffer, this.gens._get_web_audio_l_ref(), this.SAMPLING_PER_FPS);
+            this.audio_r = new Float32Array(this.gens.HEAPF32.buffer, this.gens._get_web_audio_r_ref(), this.SAMPLING_PER_FPS);
+            // input
+            this.input = new Float32Array(this.gens.HEAPF32.buffer, this.gens._get_input_buffer_ref(), this.GAMEPAD_API_INDEX);
+            // game loop
+            this.then = Date.now();
+            this.mainLoop();
+        }
+    }
+
+    mainLoop() {
+        setTimeout(this.mainLoop.bind(this), 0)
+        let now = Date.now();
+        let delta = now - this.then;
+        if (delta > this.INTERVAL && !this.pause) {
+            this.gens._tick();
+            this.then = now - (delta % this.INTERVAL);
+            this.canvasImageData.data.set(this.vram);
+            this.canvasContext.putImageData(this.canvasImageData, 0, 0);
+            this.frame++;
+            if(new Date().getTime() - this.startTime >= 1000) {
+                this.fps = this.frame;
+                this.frame = 0;
+                this.startTime = new Date().getTime();
+            }
+            // sound
+            // gens._sound();
+        }
+    }
+}
+
+module.exports = Game;
--- src/index.js
+++ src/index.js
@@ -1,119 +1,27 @@
-const wasm = require('./wasm/genplus.js');
-const fs = require('fs');
-fs.readFileSync(__dirname+'/wasm/genplus.wasm');
-
-const { createCanvas } = require('canvas');
-const ROM_PATH = './assets/roms/mk/8.bin';
-const CANVAS_WIDTH = 320;
-const CANVAS_HEIGHT = 240;
-const SAMPLING_PER_FPS = 736;
-const GAMEPAD_API_INDEX = 32;
-
-// emulator
-let gens;
-let romdata;
-let vram;
-let initialized = false;
-let pause = false;
-
-// canvas member
-let canvas;
-let canvasContext;
-let canvasImageData;
-
-// fps control
-const FPS = 60;
-const INTERVAL = 1000 / FPS;
-let now;
-let then;
-let delta;
-let startTime;
-let fps;
-let frame;
-let audio_l;
-let audio_r;
-
-(function() {
-    canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-    canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = "#00FFFF";
-    canvasContext.font = "12px monospace";
-    canvasImageData = canvasContext.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT);
-    // for fps print
-    fps = 0;
-    frame = FPS;
-    startTime = new Date().getTime();
-})();
-
-wasm().then(function(module) {
-    gens = module;
-    // memory allocate
-    gens._init();
-    console.log(gens);
-
-    const bytes = fs.readFileSync(ROM_PATH);
-    romdata = new Uint8Array(gens.HEAPU8.buffer, gens._get_rom_buffer_ref(bytes.byteLength), bytes.byteLength);
-    romdata.set(new Uint8Array(bytes));
-    // message("TOUCH HERE!");
-    initialized = true;
-    start()
-});
-
-const start = function() {
-    if(!initialized) return;
-
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    // emulator start
-    gens._start();
-    // vram view
-    vram = new Uint8ClampedArray(gens.HEAPU8.buffer, gens._get_frame_buffer_ref(), CANVAS_WIDTH * CANVAS_HEIGHT * 4);
-    // audio view
-    audio_l = new Float32Array(gens.HEAPF32.buffer, gens._get_web_audio_l_ref(), SAMPLING_PER_FPS);
-    audio_r = new Float32Array(gens.HEAPF32.buffer, gens._get_web_audio_r_ref(), SAMPLING_PER_FPS);
-    // input
-    console.log(gens._get_input_buffer_ref())
-    input = new Float32Array(gens.HEAPF32.buffer, gens._get_input_buffer_ref(), GAMEPAD_API_INDEX);
-    // game loop
-    then = Date.now();
-    loop();
-};
-
-const loop = function() {
-    setTimeout(loop, 0)
-    now = Date.now();
-    delta = now - then;
-    if (delta > INTERVAL && !pause) {
-        // update
-        gens._tick();
-        then = now - (delta % INTERVAL);
-        canvasImageData.data.set(vram);
-
-
-
-        canvasContext.putImageData(canvasImageData, 0, 0);
-        frame++;
-        if(new Date().getTime() - startTime >= 1000) {
-            fps = frame;
-            frame = 0;
-            startTime = new Date().getTime();
-        }
-        // sound
-        // gens._sound();
-    }
-};
-
-
+const Game = require('./Game');
+let game = null;
 
 const server = require('http').createServer();
+const { instrument } = require('@socket.io/admin-ui');
 const io = require('socket.io')(server, {
     cors: {
-        origin: "*",
+        //TODO: добавить домен будущего сайта/сервера
+        origin: ["http://localhost:3000", "http://localhost:8080", "https://admin.socket.io"],
+        credentials: true
     }
+});
+// https://admin.socket.io
+instrument(io, {
+    auth: false,
+    mode: "development"
 });
 
 server.listen(3000);
 
+//TODO: привязать комнаты к юзерам??
 let clients = {}
+let currentRoom = 0;
+
 function addedClient(client){
     clients[client.id] = client;
 }
@@ -122,37 +30,45 @@ function removeClient(client){
     delete clients[client.id];
 }
 
+games = {}
+
+//TODO: make it better
+function freeRoom() {
+    let numberOfClients = Object.keys(clients).length;
+    if (numberOfClients % 2 == 0) {
+        currentRoom++;
+        game = new Game();
+        game.start();
+        games["room" + currentRoom] = game;
+    }
+    return "room" + currentRoom;
+}
+
 io.on('connection', function(socket){
+    let room = freeRoom();
+    socket.join(room);
     addedClient(socket.client);
-    console.log('a user connected');
+    console.log(`User ${socket.client.id} connected to ${room}`);
     let t = setInterval(()=>{
-
-        // canvas.width = gens._get_last_width;
-        // canvas.height = gens._get_last_height;
-        canvas.width = new Int8Array(gens.HEAPF32.buffer, gens._get_last_width(), SAMPLING_PER_FPS);
-        canvas.height = new Int8Array(gens.HEAPF32.buffer, gens._get_last_height(), SAMPLING_PER_FPS);
-
-        socket.emit("frame", {"image": canvas.toDataURL(), audio_l, audio_r, fps});
+        socket.emit("frame", {"image": games[room].canvas.toDataURL(), "fps": game.fps});
     }, 0);
 
     socket.on("disconnect",()=>{
-        console.log("disconnect");
+        // TODO: Добавить удаление комнат и игр
+        console.log(`User ${socket.client.id} disconnected from ${room}`);
         removeClient(socket.client)
         clearInterval(t);
-    });
-
-    socket.on("connect",()=>{
-        console.log("connect");
     });
 
     //TEST
     let clientIndex = Object.keys(clients).indexOf(socket.client.id);
     socket.on('button', function(value){
-        input[clientIndex] = 0;
-        input[clientIndex] = value;
+        //Todo: зачем тут 0 если мы передаем 0 при отпускании?
+        games[room].input[clientIndex] = 0;
+        games[room].input[clientIndex] = value;
     })
 
     socket.on('axes', function(value, index){
-        input[index] = value;
+        games[room].input[index] = value;
     });
 });
